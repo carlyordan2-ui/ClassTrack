1c9827aaa72b5e714f5efe94041e74514a38fdde
brynmonzon55-wq <brynmonzon55@gmail.com>
2026-08-12 00:47:37 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,9 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ClassTrack/',
     plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+    ...,
     },
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
